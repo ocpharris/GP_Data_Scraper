839fff11714e885e2afd77afe2fb5ba0a42b40a0
Ollie Harris <ocpharris@gmail.com>
2023-10-19 16:12:03 +0100

    update pipelines

--- GPscraper/GPscraper/pipelines.py
+++ GPscraper/GPscraper/pipelines.py
@@ -15,15 +15,15 @@ class GpscraperPipeline:
  
         field_names = adapter.field_names()
 
-
-        for field_name in field_names:
-            value = adapter.get(field_name)
-            adapter[field_name] = value.strip()
+        
+        # for field_name in field_names:
+        #     if field_name != 'accepting_patients':
+        #         value = adapter.get(field_name)
+        #         adapter[field_name] = value.strip()
    
 
-        # name_string = adapter.get('name')
-        # adapter['name'] = name_string.strip()
-       
+        name_string = adapter.get('name')
+        adapter['name'] = name_string.strip()
 
         # # Catergory & Product Type --> switch to lowercase
         # lowercase_keys = ['category', 'product_type']
@@ -39,9 +39,15 @@ class GpscraperPipeline:
         split_string_array = strip_miles_away_string.split(' ')
         adapter['miles_away'] = float(split_string_array[0])
 
-        # # Reviews --> convert string to integer
-        # num_reviews_string = adapter.get('num_reviews')
-        # adapter['num_reviews'] = int(num_reviews_string)
+        accepting_patients_string = adapter.get('accepting_patients')
+        split_accepting_patients_string = accepting_patients_string.split(' ')
+        if split_accepting_patients_string[4] == 'not':
+            adapter['accepting_patients'] = 'no'
+        else:
+            adapter['accepting_patients']= 'yes'
+
+
+        
 
 
         # # Stars --> convert text to number 
