2fbde9dcf74027047aa3f895dc5daaee623a80f2
Ollie Harris <ocpharris@gmail.com>
2023-10-30 10:38:36 +0000

    made postcode input to use with gui

--- GPscraper/GPscraper/spiders/GPspider.py
+++ GPscraper/GPscraper/spiders/GPspider.py
@@ -1,13 +1,24 @@
 import scrapy
 from GPscraper.items import GpItem
 
+# to run in command line 'scrapy crawl GPspider -a postcode=ex55ls -o test.csv'
+
+
 class GpspiderSpider(scrapy.Spider):
     name = "GPspider"
     allowed_domains = ["www.nhs.uk"]
-    # start_urls = ['https://www.nhs.uk/service-search/find-a-gp/results/SW1V2LE?Location=SW1V2LE&Latitude=&Longitude=']
-    postcode = 'ex55ls'
-    postcode_url = 'https://www.nhs.uk/service-search/find-a-gp/results/' + postcode + '?Location=' + postcode + '&Latitude=&Longitude='
-    start_urls = [postcode_url]
+
+    # # start_urls = ['https://www.nhs.uk/service-search/find-a-gp/results/SW1V2LE?Location=SW1V2LE&Latitude=&Longitude=']
+    # postcode = 'ex55ls'
+    # postcode_url = 'https://www.nhs.uk/service-search/find-a-gp/results/' + postcode 
+    # # + '?Location=' + postcode + '&Latitude=&Longitude='
+    # start_urls = [postcode_url]
+    
+
+    def __init__(self, postcode, *args, **kwargs):
+        super(GpspiderSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [f"https://www.nhs.uk/service-search/find-a-gp/results/{postcode}"]
+
     
     def parse(self, response):
 
